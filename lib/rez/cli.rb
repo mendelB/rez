a0358e60bcb098eacd9bc872c6d781e47583118d
rezanrezano <captenrez@gmail.com>
2017-02-12 18:19:25 +0000

    update

---
+++ lib/rez/cli.rb
@@ -0,0 +1,25 @@
+class Player
+     attr_accessor :name,:age,:position
+     def initialize(name,age,position)
+        @name=name
+        @age=age
+        @position=position
+
+     end
+end
+class Cli
+    def run
+        players = [
+            marcelo = Player.new("Marcelo", 50, "Defense"),
+            ali = Player.new("Ali", 50, "Defense"),
+            gabe = Player.new("Gabe", 50, "Defense"),
+            rezan = Player.new("Rezan", 18, "Forward"),
+            mendel = Player.new("Mendel", 23, "Goalie")
+        ]
+        players.each do |i|
+            puts "Name: #{i.name}, Age: #{i.age}, Position: #{i.position}."
+        end
+
+
+    end
+end
